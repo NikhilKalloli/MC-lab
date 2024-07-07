b51d34fab1804cfa652fd0f23f860b373e6af091
NikhilKalloli <1ms22cs091@msrit.edu>
2024-07-07 16:20:46 +0530

    Add files

--- Q1_Sum.asm
+++ Q1_Sum.asm
@@ -1,17 +1,16 @@
-   	PRESERVE8
+	PRESERVE8
 	THUMB
 	AREA |.text|, CODE, READONLY
 	EXPORT __main
 	
 __main
-	movs r2,#0
-	movs r1,#10
-loop   
-	subs r1,r1,#1
-	adds r2,r1,r2
-	cmp r1, #1
-	bgt loop
-result b result
-       END
+	movs r0,#0
+	movs r1, #10
 	
-	
+loop
+	adds r0, r0,r1
+	subs r1,#1
+	cmp r1, #0
+	bne loop
+stop b stop
+	END
