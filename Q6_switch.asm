a30cb8b3266968fa59ff630bd90ff3d902248e15
NikhilKalloli <1ms22cs091@msrit.edu>
2024-07-07 18:00:48 +0530

    Add files

--- Q6_switch.asm
+++ Q6_switch.asm
@@ -1,34 +1,34 @@
-   	PRESERVE8
+	PRESERVE8
 	THUMB
 	AREA |.text|, CODE, READONLY
 	EXPORT __main
 	
 __main
-	LDR R0, =0
-	CMP R0, #3
-	BHI default_case
-	MOVS R2, #4 
-	MULS R0, R2, R0
-		LDR R1, =BranchTable
-        LDR R2,[R1,R0]
-        BX R2
+	ldr r0,= 0
+	cmp r0, #3
+	bhi default_case
+	movs r1, #4
+	muls r0, r1, r0
+		ldr r2, = BranchTable
+		ldr r1, [r2, r0]
+		bx r1
 	ALIGN 4
-
+	
 BranchTable
-	DCD Dest0 
-	DCD Dest1 
-	DCD Dest2 
-	DCD Dest3 
+	DCD Dest0
+	DCD Dest1
+	DCD Dest2
+	DCD Dest3
+	
+default_case
+stop b stop
+Dest0 ldr r0, =10
+stop1 b stop1
+Dest1 ldr r0, =20
+stop2 b stop2
+Dest2 ldr r0, =30
+stop3 b stop3
+Dest3 ldr r0, =40
+stop4 b stop4
 
-default_case 
-stop B stop
-Dest0  ldr r0, =10 
-stop1 B stop1
-Dest1 ldr r0, =20  
-stop2 B stop2
-Dest2 ldr r0, =30  
-stop3 B stop3
-Dest3 ldr r0, =40  
-stop4 B stop4
-
-	 END
+	END
